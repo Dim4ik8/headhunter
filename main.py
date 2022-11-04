c1bfb2788bac715fc44ab8ddfc58aae793ff48b3
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-11-04 20:53:34 +0300

    Find all developer vacancies

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 
 def main():
+    url = 'https://api.hh.ru/vacancies/'
+    params = {'text': 'программист', 'per_page': '100', "search_field": "name"}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-    pass
+    vacancies = response.json()
+    print(vacancies)
 
 if __name__ == '__main__':
     main()
